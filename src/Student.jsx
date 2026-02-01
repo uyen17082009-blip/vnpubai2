84e4fac6fc532de2ab17828890b5edf537941d65
uyen17082009 <uyen17082009@gmail.com>
2026-02-01 17:01:06 +0700

    Add multiple files

--- src/Student.jsx
+++ src/Student.jsx
@@ -9,6 +9,7 @@ function Student(props) {
       marginTop: '20px',
       backgroundColor: "white"
     }}>
+        <h1>THONG TIN</h1>
       <h2>Ho Ten: {props.name}</h2>
       <p>Tuoi: {props.age}</p>
       <p>Ngay Sinh: {props.birth}</p>
