90ec469bc4ae0ca3c8f5d7bcc07998f516a833bc
uyen17082009 <uyen17082009@gmail.com>
2026-02-01 17:02:20 +0700

    Add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
     <div className="container">
+      
+      <Student name="Vo Nguyen Phuong Uyen" age = {16} birth = "17/08/2009"/>
+      <Student name="Vo Nguyen Phuong Uyen" age = {16} birth = "17/08/2009"/>
       <Student name="Vo Nguyen Phuong Uyen" age = {16} birth = "17/08/2009"/>
     </div>
     </>
