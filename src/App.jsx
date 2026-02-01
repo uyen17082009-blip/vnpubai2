4b5089c53ac5aaf634d1f0f533136163152360fc
uyen17082009 <uyen17082009@gmail.com>
2026-02-01 16:44:56 +0700

    Add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Student from './Student';
 
 function Student() {
   return (
+    <>
     <div className="container">
       <Student name="Vo Nguyen Phuong Uyen" age = {16} birth = "17/08/2009"/>
     </div>
+    </>
   );
 }
 
