d65025929d48a51f54e5005c3a5f63bb39610b41
uyen17082009 <uyen17082009@gmail.com>
2026-02-01 15:56:01 +0700

    Add multiple files

---
+++ src/GioiThieu.jsx
@@ -0,0 +1,18 @@
+function Gioithieu() {
+  return (
+    <div style={{
+      textAlign: 'center', 
+      padding: '20px', 
+      border: '1px solid #ddd', 
+      borderRadius: '10px',
+      marginTop: '20px',
+      backgroundColor: "white"
+    }}>
+      <h2>Họ tên: Võ Nguyễn Phương Uyên</h2>
+      <p>Ngày sinh: 17/08/2009</p>
+      <p>Sở thích: Nghe nhạc</p>
+    </div>
+  );
+}
+
+export default Gioithieu;
